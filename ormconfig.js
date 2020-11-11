b94efcfd5dcd992468813a9a86c5bbe6c08600b9
ygoralves <ybarbozalves@gmail.com>
2020-11-11 19:45:27 -0300

    start point

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
+module.exports = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3308,
+  username: 'root',
+  password: 'consys',
+  database: 'atv1',
+  logging: true,
+  synchronize: true,
+  ssl: false,
+  logger: "advanced-console",
+  entities: ['./dist/src/models/**.entity{.ts,.js}'],
+  migrations: ['./dist/src/migrations/**/*.ts'],
+  subscribers: ['./dist/src/loaders/**/*.ts'],
+  cli: {
+    entitiesDir: './src/models',
+    migrationsDir: './src/migrations',
+    subscribersDir: './src/loaders',
+  },
+
+};
