00b5c60f5359a9d0868f9b59de88473dfa97f32a
ygoralves <ybarbozalves@gmail.com>
2020-11-13 01:35:13 -0300

    ormconfig fix and swagger tags

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ChatbotService } from './chatbot.service';
 import MessageDto from './dto/Message.dto';
 
+@ApiTags('Chatbot')
 @Controller('chatbot')
 export class ChatbotController {
   constructor(
     private chatbotService: ChatbotService
   ){}
 
+  @ApiOperation({ summary: 'Send Message to this route :D' })
   @Post('/messages')
   async getMessage(@Body() message: MessageDto): Promise<MessageDto> {
     return await this.chatbotService.getMessage(message.message);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+  .setTitle('Megahack 5.0 - Mercado Pago -  Pague Zé')
+  .setDescription('Pague Zé')
+  .setVersion('1.0')
+  .addTag('Pague Zé')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port);
+  });
 }
 bootstrap();
