1d73bddf813982b1d5844b3b4201cb30b37152a3
ygoralves <ybarbozalves@gmail.com>
2020-11-13 01:39:38 -0300

    prod directories fix

--- ormconfig.js
+++ ormconfig.js
@@ -3,12 +3,12 @@ require('dotenv').config({
 });
 
 module.exports = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3308,
-  username: 'root',
-  password: 'consys',
-  database: 'pagueze',
+  type: 'postgres',
+  host: 'ec2-34-200-106-49.compute-1.amazonaws.com',
+  port: 5432,
+  username: 'bqdtchprxxaqog',
+  password: 'd119e60f6a506daabb779ef20ff1c8d9dee06e2c8613a3c2ffe0e8a898405323',
+  database: 'd2524gct5o11f1',
   logging: true,
   synchronize: true,
   ssl: false,
