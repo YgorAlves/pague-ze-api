67d0fa16918c7a82a4b6f2dd4803addb8baa0ac1
Daniel de Souza Silva <danielolhasoroot@gmail.com>
2020-11-14 21:51:30 -0300

    generate boleto

--- src/chatbot/chatbot.service.ts
+++ src/chatbot/chatbot.service.ts
@@ -31,6 +31,15 @@ export class ChatbotService {
         term: ["seu nome","chamo", "quem é", "você", "quem"],
         res: "Opa! eu sou o Zé. Estou pronto para te ajudar, oque deseja? Pagar ou Depositar?"
       },
+      {
+        term: ["boleto", "credito", "debito"],
+        res: "Qual valor você quer depositar?"
+      },
+      {
+        term: ["reais","dinheiro","$"],
+        res: "Processado... Boleto gerado <br> 34191.79001 01043.510047 91020.150008 5 84390026000"
+      },
+
     ]
     
     //parapara  para  para !! para caralho para de digitar cacete ow demonio
